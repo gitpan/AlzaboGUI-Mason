4e06e303ff63d1c76894471d21184f0eef28e648
Dave Rolsky <autarch@urth.org>
2003-09-24 22:59:34 +0000

    Import of DROLSKY/AlzaboGUI-Mason-0.04 from CPAN.
    gitpan-cpan-distribution: AlzaboGUI-Mason
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         DROLSKY/AlzaboGUI-Mason-0.04.tar.gz
    gitpan-cpan-author:       DROLSKY
    gitpan-cpan-maturity:     released

---
+++ lib/AlzaboGUI/Mason.pm
@@ -0,0 +1,30 @@
+package AlzaboGUI::Mason;
+
+use strict;
+
+use vars qw($VERSION);
+
+$VERSION = 0.04;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+AlzaboGUI::Mason - A GUI for Alzabo using Mason
+
+=head1 SYNOPSIS
+
+  install GUI and use it ;)
+
+=head1 DESCRIPTION
+
+This module exists primarily so CPAN will index this distribution.
+
+All of the GUI functionality is implemented via Mason components (for
+now, at least).
+
+=cut
